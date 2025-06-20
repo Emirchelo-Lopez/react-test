0c9f240c1585fed45722880164b23d929a1481bb
Emirchelo-Lopez <elopezarmenincome@gmail.com>
2025-06-19 20:25:12 -0600

    Pregunta 2 resuelta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Pregunta1 from "./components/Pregunta1";
+import Pregunta2 from "./components/Pregunta2";
 
 function App() {
   return (
     <div className="App">
-      <h1>🧠 Examen React - Pregunta 1</h1>
-      <Pregunta1 />
+      <h1>🧠 Examen React - Pregunta 2</h1>
+      <Pregunta2 />
     </div>
   );
 }
